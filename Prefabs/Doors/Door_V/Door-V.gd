3909df962b928ccbec10fc6d65b0d78fa0c3dae4
AndyGFX <o.kollar@gmail.com>
2018-12-06 21:21:11 +0100

    Adde vertical Door prefab

---
+++ Prefabs/Doors/Door_V/Door-V.gd
@@ -0,0 +1,30 @@
+extends Area2D
+
+func _ready():
+	$DoorAnim.get_node("AnimationPlayer").connect("animation_finished", self, "AnimationDone")
+	pass
+
+func AnimationDone(anim_name):
+	match anim_name:
+		"Close":
+			
+			$DoorAnim.get_node("AnimationPlayer").stop()
+			pass
+			
+		"Open":	
+			$Blocker.hide()
+			$Blocker/CollisionShape2D.disabled = true
+			$DoorAnim.get_node("AnimationPlayer").stop()
+			pass
+	pass
+
+func _on_DoorV_area_entered(area):
+	$DoorAnim/AnimationPlayer.play("Open")
+	$Blocker/CollisionShape2D.disabled = false
+	print("Open door ...")
+	pass # replace with function body
+
+
+func _on_DoorV_area_exited(area):
+	$DoorAnim/AnimationPlayer.play("Close")
+	pass # replace with function body
